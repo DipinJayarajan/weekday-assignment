5952c3e4a44533061477acc93848c400eaecccb1
Dipin Nair <95134716+DipinJayarajan@users.noreply.github.com>
2024-05-03 02:31:57 +0530

    Fetched the data form the api

--- src/components/MainComponent/MainComponent.jsx
+++ src/components/MainComponent/MainComponent.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import Filter from '../Filters/Filter'
+import Cards from '../Cards/Cards'
 
 const MainComponent = () => {
+
+    const [ jobdetails, setJobDetails ] = useState([])
+    
+    const fetchJobs = async () => {
+        
+            const body = JSON.stringify({
+                "limit": 10,
+                "offset": 0
+               });
+
+        try{
+            const response = await axios.post("https://api.weekday.technology/adhoc/getSampleJdJSON", body, {
+                headers:{
+                    "Content-Type": "application/json",
+                }
+            })
+
+            const data = response.data;
+            setJobDetails(data);
+        }catch(error){
+            console.log(error, "Error Fetching Details")
+        }
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try{
+                await fetchJobs();
+            }catch(error){
+                console.log(error)
+            }
+        }
+        fetchData();
+    },[])
+
   return (
     <div>
-        <p>Heyy</p>
+
     </div>
   )
 }
