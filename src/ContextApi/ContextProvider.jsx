3db5d2a7cfe29c61b7b7c8aa761e37345d5ae94b
Dipin Nair <95134716+DipinJayarajan@users.noreply.github.com>
2024-05-04 02:43:33 +0530

    contextApi setup

---
+++ src/ContextApi/ContextProvider.jsx
@@ -0,0 +1,26 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const AppStateContext = createContext();
+
+export const useAppState = () => useContext(AppStateContext);
+
+export const AppStateProvider = ({ children }) => {
+  // const [searchTerm, setSearchTerm] = useState('');
+  // Add other states here as needed
+
+  const [ searchTerm, setSearchTerm ] = useState("")
+
+  const handleSearchTerm = (event) => {
+    setSearchTerm(event.target.value)
+  }
+
+  return (
+    <AppStateContext.Provider value={{
+      searchTerm,
+      setSearchTerm,
+      handleSearchTerm
+    }}>
+      {children}
+    </AppStateContext.Provider>
+  );
+};
--- src/components/Filters/Filter.jsx
+++ src/components/Filters/Filter.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { TextField } from '@mui/material'
+import React, { useState } from 'react'
+import "./Filter.css"
+import { useAppState } from '../../ContextApi/ContextProvider'
 
-const Filter = () => {
+const Filter = ({jobDetails}) => {
+
+  // const [ searchTerm, setSearchTerm ] = useState("")
+
+  // const handleSearchTerm = (event) => {
+  //   setSearchTerm(event.target.value)
+  // }
+
+  const { searchTerm, setSearchTerm , handleSearchTerm } = useAppState();
+
   return (
-    <div>Filter</div>
+    <div className='filter-main'>
+      <div>
+      <TextField 
+      id="outlined-search" 
+      label="Search Company" 
+      type="search" 
+      value={searchTerm}
+      onChange={handleSearchTerm}
+      />
+      </div>
+    </div>
   )
 }
 
