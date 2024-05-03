8c40352e70fcba595400e2e76e3409fb78ab44c4
Dipin Nair <95134716+DipinJayarajan@users.noreply.github.com>
2024-05-03 19:34:53 +0530

    done with the structure making of card

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,17 +1,42 @@
-import React from 'react'
-import "./Cards.css"
+import React from "react";
+import "./Cards.css";
+import { Avatar, Button, Chip } from "@mui/material";
 
-const Cards = ( {jobDetails} ) => {
+const Cards = ({ jobDetails }) => {
   return (
-    <div className='jobDetails-main'>
-        {jobDetails.map((data) => (
-                <div key={data.jdUid} className='cards-main'>
-                    {/* <p>{data.jobDetailsFromCompany} </p> */}
-                </div>
-            ))
-        }
+    <div className="jobDetails-main">
+      {jobDetails?.map((data) => (
+        <div key={data.jdUid} className="cards-main">
+          <Chip
+            label="⏳ Posted 10 days ago"
+            sx={{
+              background: "transparent",
+              boxShadow: "0rem 0rem 0.5rem #00000040",
+            }}
+            size="small"
+          />
+          <div>
+            <div>
+              <Avatar aria-label="image"></Avatar>
+              <div>
+              <p>Weekday</p>
+              <p>{data.jobRole} </p>
+              <p>{data.location}</p>
+              </div>
+            </div>
+            <p>Estimated Salary: {data.maxJdSalary} to {data.minJdSalary || ""} LPA </p>
+            <div>
+            <title>About Company :</title>
+            <p>{data.jobDetailsFromCompany} </p>
+            </div>
+            <p>Minimum Experience</p>
+            <p>{data.minExp || ""} years</p>
+          </div>
+          <Button>⚡Easy apply</Button>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Cards
+export default Cards;
--- src/components/MainComponent/MainComponent.jsx
+++ src/components/MainComponent/MainComponent.jsx
@@ -1,51 +1,53 @@
-        import axios from 'axios'
-        import React, { useEffect, useState } from 'react'
-        import Filter from '../Filters/Filter'
-        import Cards from '../Cards/Cards'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Filter from "../Filters/Filter";
+import Cards from "../Cards/Cards";
 
-        const MainComponent = () => {
+const MainComponent = () => {
+  const [jobDetails, setJobDetails] = useState([]);
 
-            const [ jobDetails, setJobDetails ] = useState([])
-            
-            const fetchJobs = async () => {
-                
-                    const body = JSON.stringify({
-                        "limit": 10,
-                        "offset": 0
-                    });
+  const fetchJobs = async () => {
+    const body = JSON.stringify({
+      limit: 10,
+      offset: 0,
+    });
 
-                try{
-                    const response = await axios.post("https://api.weekday.technology/adhoc/getSampleJdJSON", body, {
-                        headers:{
-                            "Content-Type": "application/json",
-                        }
-                    })
-
-                    const { jdList } = response.data;
-  
-                    setJobDetails(jdList);
-                }catch(error){
-                    console.log(error, "Error Fetching Details")
-                }
-            }
-
-            useEffect(() => {
-                const fetchData = async () => {
-                    try{
-                        await fetchJobs();
-                    }catch(error){
-                        console.log(error)
-                    }
-                }
-                fetchData();
-            },[])
-
-        return (
-            <div>
-                <Cards  jobDetails={jobDetails} />
-                {/* <Filter jobDetails={jobDetails} /> */}
-            </div>
-        )
+    try {
+      const response = await axios.post(
+        "https://api.weekday.technology/adhoc/getSampleJdJSON",
+        body,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
+      );
 
-        export default MainComponent
+      const { jdList } = response.data;
+
+      setJobDetails(jdList);
+    } catch (error) {
+      console.log(error, "Error Fetching Details");
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await fetchJobs();
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <Cards jobDetails={jobDetails} />
+      <Filter jobDetails={jobDetails} />
+    </div>
+  );
+};
+
+export default MainComponent;
